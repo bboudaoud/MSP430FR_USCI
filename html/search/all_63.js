73ad34d902632f5df3e63baeb4b1f41fa0640b80
Ben Boudaoud <bb3jd@virginia.edu>
2013-02-15 17:22:51 -0500

    Updated Doxygen to reflect newest code modifications

--- html/search/all_63.js
+++ html/search/all_63.js
@@ -4,7 +4,6 @@ var searchData=
   ['comm_2ec',['comm.c',['../comm_8c.html',1,'']]],
   ['comm_2eh',['comm.h',['../comm_8h.html',1,'']]],
   ['comm_5fhal_5f5342_2eh',['comm_hal_5342.h',['../comm__hal__5342_8h.html',1,'']]],
-  ['comm_5fhal_5f5342_5fh_5f',['COMM_HAL_5342_H_',['../comm__hal__5342_8h.html#afbdfe21906df17dcd9a05543dd1f9caa',1,'comm_hal_5342.h']]],
   ['comm_5fhal_5f5510_2eh',['comm_hal_5510.h',['../comm__hal__5510_8h.html',1,'']]],
   ['comm_5fhal_5f5739_2eh',['comm_hal_5739.h',['../comm__hal__5739_8h.html',1,'']]],
   ['confuca0',['confUCA0',['../comm_8h.html#ada62fa438c3ebff1a9ffc3fc9ab70928',1,'comm.h']]],
